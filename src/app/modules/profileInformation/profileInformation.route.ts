b60c32676ea449170f01670257ce7f1744b79846
senseiX21 <mahdix2118@protonmail.com>
2023-12-07 13:57:59 +0600

    Created schema for counselor

--- src/app/modules/profileInformation/profileInformation.route.ts
+++ src/app/modules/profileInformation/profileInformation.route.ts
@@ -1,12 +1,24 @@
 import express from "express";
+import { userRole } from "../../../enums/user";
+import authenticate from "../../middlewares/authenticate";
 import { ProfileInformationController } from "./profileInformation.controller";
 
 const router = express.Router();
 
 router.post('/',
+    authenticate(userRole.student, userRole.admin),
     ProfileInformationController.create);
-router.get('/', ProfileInformationController.getSingle);
-router.patch('/:id', ProfileInformationController.updateSingle);
-router.delete('/:id', ProfileInformationController.deleteSingle);
+
+router.get('/',
+    authenticate(userRole.student, userRole.admin),
+    ProfileInformationController.getSingle);
+
+router.patch('/:id',
+    authenticate(userRole.student, userRole.admin),
+    ProfileInformationController.updateSingle);
+
+router.delete('/:id',
+    authenticate(userRole.student, userRole.admin),
+    ProfileInformationController.deleteSingle);
 
 export const ProfileInformationRoutes = router;
