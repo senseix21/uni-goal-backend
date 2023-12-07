54d1fc1a2894bd37f07eb31eeda0c09a4d89dd68
senseiX21 <mahdix2118@protonmail.com>
2023-12-07 23:33:44 +0600

    Created service for studentStatus

---
+++ src/app/modules/studentStatus/studentStatus.service.ts
@@ -0,0 +1,69 @@
+import { StudentStatus } from "@prisma/client";
+import { prisma } from "../../../shared/prisma";
+
+const create = async (payload: StudentStatus, userId: string): Promise<StudentStatus> => {
+    // Check if the user has already added StudentStatus
+    const existingItem = await prisma.studentStatus.findFirst({
+        where: {
+            userId,
+        },
+    });
+
+    if (existingItem) {
+        throw new Error("Student Status  already exist for the user");
+    }
+
+
+    // If the StudentStatus is not already in the user Profile, add it
+    const result = await prisma.studentStatus.create({
+        data: {
+            ...payload,
+            userId
+        }
+    })
+    return result;
+}
+
+const getSingle = async (userId: string, id: string): Promise<StudentStatus | null> => {
+    const result = await prisma.studentStatus.findFirst({
+        where: {
+            userId,
+            id
+        },
+        include: {
+            user: true
+        }
+    })
+
+    return result;
+}
+
+
+const updateSingle = async (data: Partial<StudentStatus>, id: string): Promise<StudentStatus> => {
+    const result = await prisma.studentStatus.update({
+        where: {
+            id: id
+        },
+        data: data
+    })
+
+    return result;
+}
+
+const deleteSingle = async (id: string): Promise<StudentStatus | null> => {
+    const result = await prisma.studentStatus.delete({
+        where: {
+            id: id
+        }
+    })
+
+    return result;
+}
+
+
+export const StudentStatusService = {
+    create,
+    getSingle,
+    updateSingle,
+    deleteSingle
+}
