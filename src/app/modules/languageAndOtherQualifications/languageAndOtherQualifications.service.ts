3c8d62cc235a6af7b2b931961b10f92b40110266
senseiX21 <mahdix2118@protonmail.com>
2023-12-07 23:04:47 +0600

    Created service for languageAndOtherQualifications

---
+++ src/app/modules/languageAndOtherQualifications/languageAndOtherQualifications.service.ts
@@ -0,0 +1,70 @@
+import { LanguageAndOtherQualifications } from "@prisma/client";
+import { prisma } from "../../../shared/prisma";
+
+const create = async (payload: LanguageAndOtherQualifications, userId: string): Promise<LanguageAndOtherQualifications> => {
+    // Check if the user has already added LanguageAndOtherQualifications
+    const existingCartItem = await prisma.languageAndOtherQualifications.findFirst({
+        where: {
+            userId,
+        },
+    });
+
+    if (existingCartItem) {
+        throw new Error("Language And Other Qualifications Information already exist for the user");
+    }
+
+
+
+    // If the LanguageAndOtherQualifications is not already in the user Profile, add it
+    const result = await prisma.languageAndOtherQualifications.create({
+        data: {
+            ...payload,
+            userId
+        }
+    })
+    return result;
+}
+
+const getSingle = async (userId: string, id: string): Promise<LanguageAndOtherQualifications | null> => {
+    const result = await prisma.languageAndOtherQualifications.findFirst({
+        where: {
+            userId,
+            id
+        },
+        include: {
+            user: true
+        }
+    })
+
+    return result;
+}
+
+
+const updateSingle = async (data: Partial<LanguageAndOtherQualifications>, id: string): Promise<LanguageAndOtherQualifications> => {
+    const result = await prisma.languageAndOtherQualifications.update({
+        where: {
+            id: id
+        },
+        data: data
+    })
+
+    return result;
+}
+
+const deleteSingle = async (id: string): Promise<LanguageAndOtherQualifications | null> => {
+    const result = await prisma.languageAndOtherQualifications.delete({
+        where: {
+            id: id
+        }
+    })
+
+    return result;
+}
+
+
+export const LanguageAndOtherQualificationsService = {
+    create,
+    getSingle,
+    updateSingle,
+    deleteSingle
+}
